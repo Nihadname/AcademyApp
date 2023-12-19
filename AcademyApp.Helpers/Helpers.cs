f8816d2aaf76842b2c651ac9ceec7bc1af048d6d
Nihadname <nihadcoding@gmail.com>
2023-12-19 19:11:59 +0400

    these are done

---
+++ AcademyApp.Helpers/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyApp.Helpers
+{
+    public class Helpers
+    {
+        public static void changeTextColor(string word,ConsoleColor consoleColor)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(word);
+            Console.ResetColor();
+        }
+    }
+}
--- AcademyApp/Program.cs
+++ AcademyApp/Program.cs
@@ -1,2 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AcademyApp.Business.Services;
+using AcademyApp.Domain.Models;
+using AcademyApp.Helpers;
+StudentService studentService = new StudentService();
 Console.WriteLine("Hello, World!");
+Helpers.changeTextColor("AcademyApp",ConsoleColor.Red);
+Helpers.changeTextColor("enter number 1.createStudent 2.getAllStudents", ConsoleColor.Yellow);
+EnterMenu:string menu=Console.ReadLine();
+bool result=int.TryParse(menu, out int intMenu);
+while (true) { 
+if (result && intMenu > 0 && intMenu < 3)
+{
+        switch(intMenu)
+        {
+            case 1:
+                Helpers.changeTextColor("enter student Name", ConsoleColor.Red);
+                string name=Console.ReadLine();
+                Helpers.changeTextColor("enter student SurName", ConsoleColor.Red);
+                string Surname = Console.ReadLine();
+                Helpers.changeTextColor("enter group ", ConsoleColor.Red);
+                string Groupname = Console.ReadLine();
+                Student student=new Student();
+                
+                student.Name = name;
+                student.SurName = Surname;
+                studentService.Create(student, Groupname);
+                if (studentService.Create(student, Groupname) is null)
+                {
+                    Helpers.changeTextColor("something went wrong", ConsoleColor.Red);
+
+                }
+                else
+                {
+                    Helpers.changeTextColor("succesfuly created", ConsoleColor.Green);
+
+                }
+
+                break;
+        }
+
+}else if (intMenu == 0)
+{
+        Helpers.changeTextColor("menuyunu terk etdiniz", ConsoleColor.Green);
+        break;
+
+}
+else
+{
+    Helpers.changeTextColor("Duzgun eded daxil edin", ConsoleColor.Blue);
+        goto EnterMenu;
+}
+}
